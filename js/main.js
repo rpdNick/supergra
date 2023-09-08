e7382e1f7443f9d0776fa8b252541f7d63cd389b
rpdNick <nickyovchenko@gmail.com>
2023-09-08 18:26:26 +0300

    vacancy section

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 function initSlider(sliderElement, sliderOptions) {
-  console.log(sliderOptions)
+  console.log(sliderOptions);
   let swiper = new Swiper(sliderElement, sliderOptions);
   return swiper;
 }
 
 const providersSliderOptions = {
-  slidesPerView: 'auto',
+  slidesPerView: "auto",
   grid: {
     rows: 2,
     fill: "column",
   },
-  
+
   spaceBetween: 20,
 };
 
+const vacancySliderOptions = {
+  slidesPerView: 3,
+  spaceBetween: 16,
+
+  pagination: {
+    el: ".vacancy-slider .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".vacancy-slider .slider-button-next",
+    prevEl: ".vacancy-slider .slider-button-prev",
+  },
+};
+
 let providersSlider = initSlider(".partners-slider", providersSliderOptions);
+let vacancySlider = initSlider(".vacancy-slider", vacancySliderOptions);
