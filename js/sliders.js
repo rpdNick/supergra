7a9443f495f1d4a76a5b2fc44955b7bf9f976b1b
rpdNick <nickyovchenko@gmail.com>
2023-09-21 15:06:10 +0300

    vacancy page

---
+++ js/sliders.js
@@ -0,0 +1,67 @@
+function initSlider(sliderElement, sliderOptions) {
+  let swiper = new Swiper(sliderElement, sliderOptions);
+  return swiper;
+}
+
+const providersSliderOptions = {
+  breakpoints: {
+    320: {
+      grid: {
+        rows: 2,
+        fill: "column",
+      },
+
+      a11y: false,
+      slidesPerView: "auto",
+      spaceBetween: 8,
+    },
+
+    520: {
+      grid: {
+        rows: 2,
+        fill: "column",
+      },
+
+      a11y: false,
+      slidesPerView: "auto",
+      spaceBetween: 20,
+    },
+  },
+  updateOnWindowResize: "true",
+  pagination: {
+    el: ".partners .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".partners .slider-button-next",
+    prevEl: ".partners .slider-button-prev",
+  },
+};
+
+const vacancySliderOptions = {
+  breakpoints: {
+    320: {
+      slidesPerView: 1.03,
+      spaceBetween: 16,
+      a11y: false,
+    },
+
+    520: {
+      slidesPerView: "auto",
+      spaceBetween: 16,
+      a11y: false,
+    },
+  },
+  updateOnWindowResize: "true",
+  pagination: {
+    el: ".vacancy-slider .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".vacancy-slider .slider-button-next",
+    prevEl: ".vacancy-slider .slider-button-prev",
+  },
+};
+
+let providersSlider = initSlider(".partners-slider", providersSliderOptions);
+let vacancySlider = initSlider(".vacancy-slider", vacancySliderOptions);
